62284b8a7afd586ac5401a28cf3bfffd2976bf64
Christian <cvelazquez3@ucol.mx>
2025-04-03 10:42:51 -0600

    Agregar archivos de proyecto.

---
+++ Actividad6/Components/Data/Persona.cs
@@ -0,0 +1,9 @@
+﻿ namespace Actividad6.Components.Data
+{
+    public class Persona
+    {
+        public string? Nombre { get; set; }
+        public string? Correo { get; set; }
+        public int? Edad { get; set; }
+    }
+}
