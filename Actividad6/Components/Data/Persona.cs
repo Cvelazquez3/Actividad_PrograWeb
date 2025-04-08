435fa1019a6217f89e3a840b8680ae7f9f5e2ad3
Christian <cvelazquez3@ucol.mx>
2025-04-08 10:48:05 -0600

    Examen segunda parcial

--- Actividad6/Components/Data/Persona.cs
+++ Actividad6/Components/Data/Persona.cs
@@ -1,9 +1,19 @@
-﻿ namespace Actividad6.Components.Data
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Actividad6.Components.Data
 {
     public class Persona
     {
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string? Nombre { get; set; }
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "El correo no es valido")]
         public string? Correo { get; set; }
-        public int? Edad { get; set; }
+        [Range(15, 120, ErrorMessage = "La edad debe ser entre 15 y 120")]
+        public int Edad { get; set; }
+        [Required(ErrorMessage = "El genero es obligatorio")]
+        public string? Genero { get; set; }
     }
 }
---
+++ Actividad6/Components/Data/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad6.Components.Data
+{
+    public class Producto
+    {
+        [Required(ErrorMessage = "El número de inventario es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe ser un número entero positivo")]
+        public int NumeroInventario { get; set; }
+
+        [Required(ErrorMessage = "El nombre del producto es obligatorio")]
+        [MaxLength(200, ErrorMessage = "El nombre no puede tener más de 200 caracteres")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El departamento es obligatorio")]
+        [RegularExpression("Frutas|Verduras|Lácteos", ErrorMessage = "Debe ser Frutas, Verduras o Lácteos")]
+        public string? Departamento { get; set; }
+    }
+}
